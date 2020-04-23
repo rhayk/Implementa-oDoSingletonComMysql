7375b337ee1d07384c3c6661428a2fc65408e5b7
rhayk <42190718+rhayk@users.noreply.github.com>
2020-04-23 19:36:29 -0300

    Trabalho em desenvolvimento..

--- ConectandoSingletonComMysql/src/SingletonPool.java
+++ ConectandoSingletonComMysql/src/SingletonPool.java
@@ -1,4 +1,62 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class SingletonPool {
+	
+	private static Connection[] pool;
+	
+	private static int index;
+	private static int length = 5;
+	
+	private SingletonPool() {
+	}
+	
+	public static Connection getConnection() throws Exception {
+		if(pool == null) {
+			synchronized (SingletonPool.class) {
+				if(pool == null)
+					pooll();
+			}
+			
+		}
+		
+		index = (index + 1) % length;
+		return pool[index];
+		
+	}
+	
+	private static void pooll() throws Exception {
+		Properties props = loadProperties();
+		String url = props.getProperty("dburl");
+		
+		pool = new Connection[length];
+		
+		for(int i = 0; i < length; i++) {
+			try {
+				pool[i] = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new Exception(e.getMessage());
+			}
+			
+		}
+		
+	}
 
+	private static Properties loadProperties() throws Exception {
+		try(FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch(IOException e) {
+			throw new Exception(e.getMessage());
+		}
+		
+	}
+	
 }
--- ConectandoSingletonComMysql/src/SingletonPoolTeste.java
+++ ConectandoSingletonComMysql/src/SingletonPoolTeste.java
@@ -1,4 +1,12 @@
+import java.sql.Connection;
 
 public class SingletonPoolTeste {
 
+	public static void main(String[] args) throws Exception {
+		
+		Connection pool = SingletonPool.getConnection();
+	
+		System.out.println(pool);
+	
+	}
 }
